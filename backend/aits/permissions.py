fd4e83232f4571739bf7dd721658563fc89ef0b6
sandrasamara <sandranagawa30@gmail.com>
2025-03-11 13:58:47 +0300

    Update aits

--- backend/aits/permissions.py
+++ backend/aits/permissions.py
@@ -2,3 +2,17 @@ from rest_framework import permissions
 class IsRegistrar(permissions.BasePermission):
     def has_permissions(self,request,view):
         return request.user.is_authenticated and request.user.user_type=='registrar'
+class Islecturer(permissions.BasePermission):
+    def has_permissions(self,request,view):
+        return request.user.is_authenticated and request.user.user_type=='lecturer'
+class Isstudent(permissions.BasePermissions):
+    def has_permissions(self,request,view):
+        return request.user.is_authenticated and request.user.user_type=='student'
+class IsOwnerOrReadOnly(permissions.BasePermissions):
+    #it only allows the user who has submitted the issue to update or delete the issue
+    def has_object_permissions(self,request,view,obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.student==request.user
+
+
