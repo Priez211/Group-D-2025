29fe4d42498f94ffca2a78537c27cc8bc25e83b6
sandrasamara <sandranagawa30@gmail.com>
2025-03-11 13:34:10 +0300

    Update aits

--- backend/aits/views.py
+++ backend/aits/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framwork import viewsets,permisssions
+from .models import Issue
+from rest_framework.permissions import IsAuthenticated
+from .serializers import IssueSerializer,UserSerializer
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from .permissions import IsRegistrar,Islecturer,Isstudent
+from rest_framework.authentication import TokenAuthentication
+
+User=get_user_model()
+class UserViewSet(viewsets.ModelViewSet):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+    permission_classes=[permissions.IsAuthenticated,IsRegistrar]#only registrars can manage the users
 
 # Create your views here.
