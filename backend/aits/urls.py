c58732e9d3c26b694a83873fe41fd73f4602ed38
sandrasamara <sandranagawa30@gmail.com>
2025-03-14 00:57:15 +0300

    Update urls

--- backend/aits/urls.py
+++ backend/aits/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet,IssueViewSet,DepartmentViewSet
+from .views import UserViewSet,IssueViewSet,DepartmentViewSet,NotificationsViewSet
 
 
 router=DefaultRouter()
 router.register(r'users',UserViewSet)
 router.register(r'issues',IssueViewSet)
 router.register(r'departments',DepartmentViewSet)
+router.register(r'Notifications',NotificationsViewSet,basename='notifications')
 
 urlpatterns=[
     path('',include(router.urls))
