2625273bc13a0b1f4e390da17a7c3e2c96694ec1
sandrasamara <sandranagawa30@gmail.com>
2025-03-26 13:32:54 +0300

    retrieving urls

--- backend/aits/urls.py
+++ backend/aits/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet,IssueViewSet,DepartmentViewSet,NotificationsViewSet
+from .views import UserViewSet,IssueViewSet,DepartmentViewSet,NotificationsViewSet,LoginView
+
+
+router=DefaultRouter()
+router.register(r'departments',DepartmentViewSet)
+router.register(r'users',UserViewSet)
+router.register(r'issues',IssueViewSet)
+router.register(r'Notifications',NotificationsViewSet,basename='notifications')
 
 urlpatterns=[
-    
+    path('',include(router.urls)),
+    path('api/Login/',LoginView.as_view(),name='Login'),
 ]
