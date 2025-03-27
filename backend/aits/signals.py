d5726709a6d29db33fdde529e93c123b1a14c444
sandrasamara <sandranagawa30@gmail.com>
2025-03-27 19:26:29 +0300

    Add and update signals

---
+++ backend/aits/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Issue,Notifications
+
+@receiver(post_save,sender=Issue)
+def issue_nitification(sender,instance,created,**kwargs):
+    if created:
+        message=f"A new issue'{instance.title}'has been created"
+        notification_type="New Issue Created"
+    else:
+        message=f"Your issue'{instance.title}'has been updated."
+        notification_type="Issue Updated"
+    Notifications.objects.create(
+        receipient=instance.student,
+        notification_type=notification_type,
+        message=message
+
+    )
