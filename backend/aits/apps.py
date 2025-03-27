91fb43fe19942152c6a4fe47470a6519ff64d7dd
sandrasamara <sandranagawa30@gmail.com>
2025-03-27 19:25:32 +0300

    add apps

--- backend/aits/apps.py
+++ backend/aits/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class AitsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'aits'
+    def ready(self):
+        import aits.signals
