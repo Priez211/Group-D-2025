c7d287182b6d61152b3f4119597dc331c9be8c7c
Nambirige-Eron <eronnambirige@gmail.com>
2025-04-28 16:47:20 +0300

    Update IssueDetail.jsx by adding comments

--- frontend/src/components/IssueDetail.jsx
+++ frontend/src/components/IssueDetail.jsx
@@ -1,3 +1,4 @@
+//the issue details page
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { getIssueById } from '../services/api';
