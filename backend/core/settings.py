0c428f903c1dd8ba084143b67ab3db7a1be56df3
sandrasamara <sandranagawa30@gmail.com>
2025-04-28 18:00:40 +0300

    Add database

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'aits_db',
+        'USER':'postgres',
+        'PASSWORD':'5432',
+        'HOST':'localhost',
+        'PORT':'5432', 
     }
 }
 
