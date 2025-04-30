cfb229b7e40a7624b8576e2c0d79c4d18e2aaee2
nyakana patricia <nyakanapatriciaamooti@gmail.com>
2025-05-01 01:17:30 +0300

    models

--- backend/users/models.py
+++ backend/users/models.py
@@ -36,7 +36,7 @@ class User(AbstractUser):
     )
     
     email = models.EmailField(unique=True)
-    full_name = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=300)
     user_id = models.CharField(max_length=50, unique=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
     
