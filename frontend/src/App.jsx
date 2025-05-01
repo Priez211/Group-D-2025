2bb22a658519e281d2e2dfaa80a204a93c1bb783
Nambirige-Eron <eronnambirige@gmail.com>
2025-05-01 21:51:36 +0300

    Update App.jsx with new code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,8 +10,16 @@ import Notifications from './components/Notifications';
 // Import registrar components
 import RegistrarDashboard from './components/registrar/RegistrarDashboard';
 import StudentManagement from './components/registrar/StudentManagement';
+import LecturerManagement from './components/registrar/LecturerManagement';
 import DepartmentManagement from './components/registrar/DepartmentManagement';
+import RegistrarNotifications from './components/registrar/RegistrarNotifications';
+import RegistrarIssues from './components/registrar/RegistrarIssues';
+// Import lecturer components
+import LecturerDashboard from './components/lecturer/LecturerDashboard';
+import LecturerIssueManagement from './components/lecturer/LecturerIssueManagement';
+import LecturerNotifications from './components/lecturer/LecturerNotifications';
 import './App.css';
+import { NotificationProvider } from './context/NotificationContext';
 
 const PrivateRoute = ({ children }) => {
   const isAuthenticated = localStorage.getItem('token');
@@ -21,95 +29,179 @@ const PrivateRoute = ({ children }) => {
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          {/* Redirect root to login */}
-          <Route path="/" element={<Navigate to="/login" />} />
-          
-          {/* Auth routes */}
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          
-          {/* Protected routes */}
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <StudentDashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/add-issue"
-            element={
-              <PrivateRoute>
-                <AddNewIssue />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/my-issues"
-            element={
-              <PrivateRoute>
-                <MyIssues />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/issue/:issueId"
-            element={
-              <PrivateRoute>
-                <IssueDetail />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/add-issue/:issueId"
-            element={
-              <PrivateRoute>
-                <AddNewIssue />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/notifications"
-            element={
-              <PrivateRoute>
-                <Notifications />
-              </PrivateRoute>
-            }
-          />
-          
-          {/* Registrar routes */}
-          <Route
-            path="/registrar"
-            element={
-              <PrivateRoute>
-                <RegistrarDashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/registrar/students"
-            element={
-              <PrivateRoute>
-                <StudentManagement />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/registrar/departments"
-            element={
-              <PrivateRoute>
-                <DepartmentManagement />
-              </PrivateRoute>
-            }
-          />
-          
-          {/* Catch all route */}
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-      </div>
+      <NotificationProvider>
+        <div className="App">
+          <Routes>
+            {/* Redirect root to login */}
+            <Route path="/" element={<Navigate to="/login" />} />
+            
+            {/* Auth routes */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            
+            {/* Student routes */}
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <StudentDashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/add-issue"
+              element={
+                <PrivateRoute>
+                  <AddNewIssue />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/add-issue/:issueId"
+              element={
+                <PrivateRoute>
+                  <AddNewIssue />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/my-issues"
+              element={
+                <PrivateRoute>
+                  <MyIssues />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/issue/:issueId"
+              element={
+                <PrivateRoute>
+                  <IssueDetail />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/notifications"
+              element={
+                <PrivateRoute>
+                  <Notifications />
+                </PrivateRoute>
+              }
+            />
+            
+            {/* Registrar routes */}
+            <Route
+              path="/registrar"
+              element={
+                <PrivateRoute>
+                  <RegistrarDashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/registrar/students"
+              element={
+                <PrivateRoute>
+                  <StudentManagement />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/registrar/lecturers"
+              element={
+                <PrivateRoute>
+                  <LecturerManagement />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/registrar/departments"
+              element={
+                <PrivateRoute>
+                  <DepartmentManagement />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/registrar/issues"
+              element={
+                <PrivateRoute>
+                  <RegistrarIssues />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/registrar/issues/:issueId"
+              element={
+                <PrivateRoute>
+                  <IssueDetail />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/registrar/notifications"
+              element={
+                <PrivateRoute>
+                  <RegistrarNotifications />
+                </PrivateRoute>
+              }
+            />
+            
+            {/* Lecturer routes */}
+            <Route
+              path="/lecturer-dashboard"
+              element={
+                <PrivateRoute>
+                  <LecturerDashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/lecturer/issues"
+              element={
+                <PrivateRoute>
+                  <LecturerIssueManagement />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/lecturer/issues/:issueId"
+              element={
+                <PrivateRoute>
+                  <IssueDetail />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/lecturer/dashboard"
+              element={
+                <PrivateRoute>
+                  <LecturerDashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/lecturer/issues/:issueId"
+              element={
+                <PrivateRoute>
+                  <IssueDetail />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/lecturer/notifications"
+              element={
+                <PrivateRoute>
+                  <LecturerNotifications />
+                </PrivateRoute>
+              }
+            />
+            
+            {/* Catch all route */}
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </div>
+      </NotificationProvider>
     </Router>
   );
 }
