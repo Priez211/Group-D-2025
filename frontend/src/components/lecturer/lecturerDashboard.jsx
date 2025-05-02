f17b57b2a41c8bbb54b94e5cbf2f8f56c644393d
Jollyneflavia <Jollyneflavia@gmail.com>
2025-05-02 11:27:49 -0700

    Update lecturerDashboard.jsx

--- frontend/src/components/lecturer/lecturerDashboard.jsx
+++ frontend/src/components/lecturer/lecturerDashboard.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { getLecturerIssues } from '../../services/api';
 import UserProfile from '../UserProfile';
 import NotificationBadge from '../NotificationBadge';
+import '../../styles/Dashboard.css';
 const lecturerDashboard = () => {
   return (
     <div>lecturerDashboard</div>
