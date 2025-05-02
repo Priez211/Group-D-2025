dd5d8990ac62ab79f0af7d55bce1810ff87ae60a
Jollyneflavia <Jollyneflavia@gmail.com>
2025-05-02 11:23:58 -0700

    Update lecturerDashboard.jsx
    improving lecturer dashboard

--- frontend/src/components/lecturer/lecturerDashboard.jsx
+++ frontend/src/components/lecturer/lecturerDashboard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import { getLecturerIssues } from '../../services/api';
+import UserProfile from '../UserProfile';
 const lecturerDashboard = () => {
   return (
     <div>lecturerDashboard</div>
