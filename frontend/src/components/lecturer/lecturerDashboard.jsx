1953f840c04186297143e58470c5096f8e4a55e5
Jollyneflavia <Jollyneflavia@gmail.com>
2025-05-03 17:51:01 -0700

    Update lecturerDashboard.jsx

--- frontend/src/components/lecturer/lecturerDashboard.jsx
+++ frontend/src/components/lecturer/lecturerDashboard.jsx
@@ -4,6 +4,10 @@ import { getLecturerIssues } from '../../services/api';
 import UserProfile from '../UserProfile';
 import NotificationBadge from '../NotificationBadge';
 import '../../styles/Dashboard.css';
+
+
+const API_URL = 'http://localhost:8000';
+
 const lecturerDashboard = () => {
   return (
     <div>lecturerDashboard</div>
