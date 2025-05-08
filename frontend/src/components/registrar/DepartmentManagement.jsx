60f268f3fc2e33d8506f49701d54e7159900eff6
Nambirige-Eron <eronnambirige@gmail.com>
2025-05-08 21:11:37 +0300

    Updated DepartmentManagement.jsx

--- frontend/src/components/registrar/DepartmentManagement.jsx
+++ frontend/src/components/registrar/DepartmentManagement.jsx
@@ -1,3 +1,4 @@
+// for enabling the registrar to handle different departments
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import UserProfile from '../UserProfile';
