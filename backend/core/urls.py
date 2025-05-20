ea08a4de89d581f5662f03f94d865b8342702606
Malual Biar <biaralier1@gmail.com>
2025-05-21 00:04:30 +0300

    changes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,13 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('aits.urls')),
     path('api/notifications/', include('notifications.urls')),
+    path('', RedirectView.as_view(url='api/')),  # Redirect root to /api/
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
