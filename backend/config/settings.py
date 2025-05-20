a05f89d430fe2e1b89b63e7ed04a5e03f330289b
Malual Biar <biaralier1@gmail.com>
2025-05-21 01:06:20 +0300

    fixing backend urls

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,6 +34,28 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
+# Additional CORS settings
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 # Custom user model
 AUTH_USER_MODEL = 'users.User'
 
