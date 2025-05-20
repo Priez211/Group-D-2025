a44dedcf71c5c0b630d000143ff5f34da32c42d1
sandrasamara <sandranagawa30@gmail.com>
2025-05-20 14:18:49 +0300

    Changes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -8,3 +8,4 @@ urlpatterns = [
     path('api/', include('aits.urls')),
     path('api/notifications/', include('notifications.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
