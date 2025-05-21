f5f7cf3ca38c8a2d471d4b84c892f8801640edb9
sandrasamara <sandranagawa30@gmail.com>
2025-05-21 18:50:32 +0300

    changes

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-o-nutq5r+=2q=7c^p)kaf
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 # Ensure Railway domains are allowed in production
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,*.up.railway.app,group-d-2025-2.onrender.com').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,*.up.railway.app').split(',')
 
 
 # Application definition
